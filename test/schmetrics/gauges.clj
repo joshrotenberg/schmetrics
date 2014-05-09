77a0b0b094f7d9efefe3e73655e61631263f33a9
josh rotenberg <joshrotenberg@gmail.com>
2014-05-09 15:15:57 -0700

    rm random file

--- test/schmetrics/gauges.clj
+++
@@ -1,8 +0,0 @@
-(ns schmetrics.gauge-test
-  (:require [clojure.test :refer :all]
-            [schmetrics.gauge :as gauge]))
-
-
-(deftest gauge-test
-  (testing "gauge read"
-    (println (gauge/retrieve-gauge "foo"))))
