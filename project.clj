2d323b31085823b187263bdc6c38111571a34444
josh rotenberg <joshrotenberg@gmail.com>
2015-10-22 14:32:15 -0700

    add test profiles for other clojure versions

--- project.clj
+++ project.clj
@@ -6,13 +6,18 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [io.dropwizard.metrics/metrics-core "3.1.2"]
                  [io.dropwizard.metrics/metrics-json "3.1.2"]
-                 [io.dropwizard.metrics/metrics-healthchecks "3.1.2"]
-                 [cheshire "5.3.1" :scope "test"]]
+                 [io.dropwizard.metrics/metrics-healthchecks "3.1.2"]]
   :plugins [[codox "0.8.0"]]
-  :profiles {:dev
-             {:plugins [[lein-cloverage "1.0.6"]]}}
+  :profiles {:test {:dependencies [[cheshire "5.3.1"]]
+                    :plugins [[lein-cloverage "1.0.6"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
+             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :dev [:1.7 :test]
+             }
   :codox {:src-dir-uri "http://github.com/joshrotenberg/schmetrics/blob/master/"
-          :src-linenum-anchor-prefix "L"})
+          :src-linenum-anchor-prefix "L"}
+  :aliases {"test-all"   ["with-profile" "+1.5:+1.6:+1.7" "test"]})
 
 
 
